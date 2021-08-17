a21931ec4b46f4755fccfc850c286df51dd24829
AnneCode <anneliu49@gmail.com>
2021-08-17 16:10:06 -0400

    rebuild project

--- src/services/search.js
+++
@@ -1,6 +0,0 @@
-// FIXME:value在models里面的search也没用到呀
-export function getListsFromAPI(value) {
-  return fetch('/api/getLists?value=' + value)
-    .then(res => res.json())
-    .catch(err => console.log(err));
-}

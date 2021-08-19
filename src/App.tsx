85d63ac5dc89770ee9abb3701aa8ac96e7aee608
AnneCode <anneliu49@gmail.com>
2021-08-19 16:40:57 -0400

    feat: add carousel and sideMenu components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import logo from './assets/logo.svg';
 import styles from "./App.module.css";
-import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
-import { GlobalOutlined } from "@ant-design/icons";
-import {Header, Footer} from './components'
+import {Header, Footer,Carousel, SideMenu} from './components'
 import {Row, Col} from "antd";
 function App() {
   return (
@@ -12,8 +10,8 @@ function App() {
       <div className={styles['page-content']}>
         <Row style={{marginTop: 20}}>
           {/* NOTE：多重菜单 */}
-          <Col span={6} style={{background:"red"}}>多重菜单</Col>
-          <Col span={18} style={{backgroundColor:'blue'}}>走马灯</Col>
+          <Col span={6} style={{background:"red"}}><SideMenu /></Col>
+          <Col span={18} style={{backgroundColor:'blue'}}><Carousel/></Col>
         </Row>
 
       </div>
--- src/components/sideMenu/sideMenu.tsx
+++ src/components/sideMenu/sideMenu.tsx
@@ -6,6 +6,27 @@ import { GifOutlined } from "@ant-design/icons";
 import { Menu } from 'antd';
 export const SideMenu: React.FC =() => {
     return(
+    <Menu mode='vertical' className={styles['side-menu']}>
+        {/* NOTE: React规定，循环生成的组件必须加上Key */}
+        {sideMenuList.map((m,index) => (
+            <Menu.SubMenu
+                key={`side-menu-${index}`}
+                title={<span><GifOutlined />{m.title}</span>}
+            >
+                {m.subMenu.map((sm, smindex) => (
+                    <Menu.SubMenu key={`side-menu-${smindex}`} title={
+                        <span><GifOutlined />{sm.title}</span>
+                    }>
+                        {sm.subMenu.map((sms,index) => (
+                            <Menu.Item key={index}>
+                                <span><GifOutlined/>{sms}</span>
+                            </Menu.Item>
+                        ))}
+                    </Menu.SubMenu>
+                ))}
+            </Menu.SubMenu>
+        ))}
+    </Menu>
         
     )
 }

9e67dd25e02474ac517767fc10d2dc6015e82389
AnneCode <anneliu49@gmail.com>
2021-08-19 16:15:19 -0400

    crate sideMenu component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import styles from "./App.module.css";
 import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
 import {Header, Footer} from './components'
+import {Row, Col} from "antd";
 function App() {
   return (
     <div className={styles.App}>
       <Header/>
+      <div className={styles['page-content']}>
+        <Row style={{marginTop: 20}}>
+          {/* NOTE：多重菜单 */}
+          <Col span={6} style={{background:"red"}}>多重菜单</Col>
+          <Col span={18} style={{backgroundColor:'blue'}}>走马灯</Col>
+        </Row>
+
+      </div>
       <Footer/>
     </div>
   );
---
+++ src/components/sideMenu/sideMenu.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import styles from './SideMenu.module.css';
+import {sideMenuList} from './mockup';
+import { GifOutlined } from "@ant-design/icons";
+import { Menu } from 'antd';
+export const SideMenu: React.FC =() => {
+    return(
+        
+    )
+}

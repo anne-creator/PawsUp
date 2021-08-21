4943be6cc8bc8cca87ac470b1a4d0bc316337bbe
AnneCode <anneliu49@gmail.com>
2021-08-21 16:15:34 -0400

    feat: multi page switch

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,10 @@ function App() {
     <div className={styles.App}>
       {/* NOTE:  包裹所有的页面*/}
       <BrowserRouter>
-        <Route path='/' component={HomePage} />
-
+        <Route exact path='/' component={HomePage} />
+        {/* NOTE：路由发生了变化，两个页面会堆叠显示。所以需要短路处理，给path之前加入 exact  */}
+        <Route exact path='/signin' render={() => <h1>登录页面</h1>} />
+        <Route render={() => <h1>404: can't find page.</h1>} />
       </BrowserRouter>
     </div>
   );

a917cdb4b82e9a2d45d50c23d6bb3049af0cbd39
AnneCode <anneliu49@gmail.com>
2021-08-23 14:31:11 -0400

    refactor: introduce material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import 'antd/dist/antd.css'
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

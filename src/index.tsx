68800e948e4d400f771d07931a22ca97a8a7bf82
AnneCode <anneliu49@gmail.com>
2021-08-23 14:39:57 -0400

    delete material UI and back to antd

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import 'antd/dist/antd.css'
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')

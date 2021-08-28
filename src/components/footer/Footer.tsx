5d2a127c74779afff43e88f1953d6ed514061db1
AnneCode <anneliu49@gmail.com>
2021-08-28 14:14:09 -0400

    footer done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import styles from 'Footer.module.scss';
-import { Layout, Typography } from "antd";
+import styles from './Footer.module.scss';
+import { Divider } from "antd";
+import logo from '../../assets/logo.svg';
+
 export const Footer: React.FC = () => {
-    return (
-        <Layout.Footer>
-            <Typography.Title level={3} style={{ textAlign: 'center' }}>
-                版权所有 @ React 旅游网
-            </Typography.Title>
-        </Layout.Footer>
-    )
+   return (
+      <footer className={styles['footer-container']}>
+         <div className={styles['footer']}>
+            <div className={styles['footer__main']}>
+               <div className={styles['footer__paws-up']}>
+                  <div className={styles['footer__paws-up__logo']}>
+                     <img src={logo} alt="logo" className={styles["footer__paws-up__img"]} />
+                     <h1 className={styles["footer__paws-up__title"]}>Paws Up</h1>
+                  </div>
+                  <div className={styles['footer__paws-up__save-life']}>
+                     Let’s saves lives together!
+                  </div>
+               </div>
+               <div className={styles['footer__nav']}>
+                  <div className={styles['footer__nav__item']}>Adopt</div>
+                  <div className={styles['footer__nav__item']}>Donate</div>
+                  <div className={styles['footer__nav__item']}>Order</div>
+                  <div className={styles['footer__nav__item']}>Profile</div>
+               </div>
+               <div className={styles['footer__info']}>
+                  <div className={styles['footer__info__item']}>Contact</div>
+                  <div className={styles['footer__info__item']}>FAQ</div>
+                  <div className={styles['footer__info__item']}>Join Us</div>
+               </div>
+            </div>
+            <Divider />
+            <div className={styles['footer__copyright']}>
+               <div className={styles['footer__copyright__content']}>
+                  <p className={styles['footer__copyright__Paws-Up']}>Copyright © 2021 Anne Paws Up. All rights reserved</p>
+                  <p className={styles['footer__copyright__cookies']} >We use cookies for better service.<span className={styles['footer__copyright__span']}>Accept</span></p>
+               </div>
+            </div>
+         </div>
+      </footer>
+   )
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Header, Footer } from '../../components'
 import { Button } from 'antd'
+import { ArrowRightOutlined } from '@ant-design/icons';
 import way1 from '../../assets/images/way1.png'
 import way2 from '../../assets/images/way2.png'
 import way3 from '../../assets/images/way3.png'
 import way4 from '../../assets/images/way4.png'
+import dogFood from '../../assets/images/dogFood.png'
+
+
 
 
 // import { Row, Col, Typography } from "antd";
@@ -14,7 +18,9 @@ import way4 from '../../assets/images/way4.png'
 // import sideImage3 from './../../assets/images/sider_2019_02-04-2.png';
 import styles from './HomePage.module.scss'
 export class HomePage extends React.Component {
+
   render() {
+    const seniorHref = 'https://petsmartcharities.ca/our-stories/community-impact/keeping-pets-in-the-arms-of-seniors-who-love-them';
     return (
       <div className={styles.App}>
         <Header />
@@ -56,22 +62,71 @@ export class HomePage extends React.Component {
 
         {/* /////////////// content /////////////// */}
         <div className={styles['content']}>
+          {/* ////  1 video  //// */}
           <div className={styles['content__video']}>
             <div className={`${styles['content__video__img']} ${styles['item']}`} >
             </div>
             <div className={`${styles['text']} ${styles.item}`}>
-              <div className={styles['text__decor']}>New ways of  helping animals </div>
+              <div className={styles['text__decor']}>New ways of helping animals </div>
               <div className={styles['text__heading']}>Like watching cute animal viedos? </div>
               <div className={styles['text__subheading']}> Donate selected products, We will deliver the cute animal video.</div>
               <div className={styles['text__restriction']}></div>
               <Button className={styles['text__button']} >Discover more</Button>
             </div>
           </div>
+          {/* ////  2 buy  //// */}
           <div className={styles['content__buy']}>
-            <div className={`${styles['content__buy__text']} ${styles.item}`}>
-
+            <div className={`${styles['text']} ${styles.item}`}>
+              <div className={styles['text__decor']}>New ways of helping animals </div>
+              <div className={styles['text__heading']}>Aready got a pet? You buy some products, we donate 5% in rescuing.</div>
+              <div className={styles['text__subheading']}>Every purchasing of your own pet will donate 5% to help animal recuing.</div>
+              <div className={styles['text__restriction']}></div>
+              <Button className={styles['text__button']} >Discover more</Button>
             </div>
             <div className={`${styles['content__buy__img']} ${styles.item}`}></div>
+          </div>
+
+
+          {/* ////  3 feed  //// */}
+          <div className={styles['content__feed']}>
+            <div className={`${styles['content__feed__img']} ${styles.item}`}></div>
+            <div className={`${styles['text']} ${styles.item}`}>
+              <div className={styles['text__heading']}>Help to feed the hungry</div>
+              <div className={styles['text__subheading']}>
+                <p>Food donations are always the core of caving our furry friends. Your donation is distributed directly to a furry friend in need of a full belly.</p>
+
+                <p>Only $30 can feed a medium size dog for a month with quality food. And guess what, $30 can feed two cats!</p>
+              </div>
+              <div className={styles['text__restriction']}></div>
+              <img className={styles['text__img']} src={dogFood} />
+              <Button type='primary' className={styles['button__primary__donate']} >Donate $30</Button>
+              <Button className={styles['button__secondary__donate']} >Explore more</Button>
+            </div>
+          </div>
+          {/* ////  4 sick  //// */}
+          <div className={styles['content__sick']}>
+            <div className={`${styles['text']} ${styles.item}`}>
+              <div className={styles['text__decor']}>New ways of helping animals </div>
+              <div className={styles['text__heading']}>Aready got a pet? You buy some products, we donate 5% in rescuing.</div>
+              <div className={styles['text__subheading']}>Every purchasing of your own pet will donate 5% to help animal recuing.</div>
+              <div className={styles['text__restriction']}></div>
+              <Button type='primary' className={styles['button__primary__donate']} >Donate  $50 </Button>
+              <Button className={styles['button__secondary__donate']} >Expolore More</Button>
+            </div>
+            <div className={`${styles['content__sick__img']} ${styles.item}`}></div>
+          </div>
+          {/* ////  5 senior  //// */}
+          <div className={styles['content__senior']}>
+            <div className={`${styles['content__senior__img']} ${styles.item}`}></div>
+            <div className={`${styles['text']} ${styles.item}`}>
+              <div className={styles['text__heading']}>Keeping Pets in the Arms of Seniors</div>
+              <div className={styles['text__subheading']}>
+                <p>“
+                  Pets offer more than companionship to many Canadian seniors. They also act as a source of purpose, affection, empathy, and have a…
+                  ”</p>
+              </div>
+              <Button className={styles['button__senior']} type='primary' shape='round' href={seniorHref}>Read More <ArrowRightOutlined /></Button>
+            </div>
           </div>
         </div>
         {/* <ProductCollection

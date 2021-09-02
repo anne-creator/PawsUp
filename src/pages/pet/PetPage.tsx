f8684fe9e041a499a727b481b08d787349ab3bf9
AnneCode <anneliu49@gmail.com>
2021-09-02 12:51:41 -0400

    pet and adopt page UI done

--- src/pages/pet/PetPage.tsx
+++ src/pages/pet/PetPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './PetPage.module.scss'
 import { Button, Divider } from 'antd'
+import { ArrowRightOutlined } from "@ant-design/icons";
+
 
 
 // export interface PetPageProps {
@@ -11,8 +13,12 @@ export const PetPage: React.FC = () => {
         <div className={styles['pet']}>
             <main className={styles['pet__detail']}>
                 <div className={styles['pet__header']}>
+                    <div className={styles['pet__header__img']} ></div>
                     <div className={styles['pet__header__top']}>
-                        <h1>Luna</h1>
+                        <div>
+                            <h1>Luna</h1>
+                            <div className={styles['pet__header__top__name-decor']}></div>
+                        </div>
                         <Button>Adopt FAQ </Button>
                     </div>
                     <div className={styles['pet__header__down']}>Kitten  |  Male  |  Medium  |  Black</div>
@@ -28,17 +34,21 @@ export const PetPage: React.FC = () => {
                     </div>
                 </div>
                 <Divider />
-                <div className={styles['pet__detail']}>
+                <div className={styles['pet__describe']}>
                     <h2>Meet Luna</h2>
                     <div className={styles['pet__detail__content']}>
                         <p>This is Luna. He is one of the trio rescued from outdoors. He is a little shy like his brother, Poppy but is still playful and cuddly. He would really like to be adopted with one of his siblings if possible or to a home with another young cat.</p>
                         <p>Poppy, Piper and Luna are available for adoption through Grand River All Breed Rescue. The adoption donation is $200 per cat. All of our cats are spayed or neutered, microchipped, and vaccinated as part of their adoption. If you’d like to adopt, please fill out an application. You can also reach us by email at GrandRiverRescue@hotmail.com.</p>
                     </div>
                 </div>
+                <Divider />
+                <Button className={styles['pet__button']}>Adopt Luna <ArrowRightOutlined className={styles['pet__button__arrow']} /></Button>
             </main>
             <div className={styles['pet__side']}>
-                <div className={styles['pet__photo']}></div>
-                <div className={styles['pet__sponsor']}></div>
+                <div className={styles['pet__side__sponsor']}>
+                    <h4>Help provide for Luna’s Care</h4>
+                    <Button className={styles['button']}>Sponsor Luna</Button>
+                </div>
             </div>
         </div>
     )
--- src/pages/pet/dist/PetPage.js
+++ src/pages/pet/dist/PetPage.js
@@ -4,14 +4,18 @@ exports.PetPage = void 0;
 var react_1 = require("react");
 var PetPage_module_scss_1 = require("./PetPage.module.scss");
 var antd_1 = require("antd");
+var icons_1 = require("@ant-design/icons");
 // export interface PetPageProps {
 // }
 exports.PetPage = function () {
     return (react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet'] },
         react_1["default"].createElement("main", { className: PetPage_module_scss_1["default"]['pet__detail'] },
             react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__header'] },
+                react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__header__img'] }),
                 react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__header__top'] },
-                    react_1["default"].createElement("h1", null, "Luna"),
+                    react_1["default"].createElement("div", null,
+                        react_1["default"].createElement("h1", null, "Luna"),
+                        react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__header__top__name-decor'] })),
                     react_1["default"].createElement(antd_1.Button, null, "Adopt FAQ ")),
                 react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__header__down'] }, "Kitten  |  Male  |  Medium  |  Black")),
             react_1["default"].createElement(antd_1.Divider, null),
@@ -23,12 +27,17 @@ exports.PetPage = function () {
                     react_1["default"].createElement("p", { className: PetPage_module_scss_1["default"]['pet__about__content-item'] }, "GOOD IN A HOME WITH:\u00A0\u00A0\u00A0\u00A0Other cats"),
                     react_1["default"].createElement("p", { className: PetPage_module_scss_1["default"]['pet__about__content-item'] }, "Coat Length:\u00A0\u00A0\u00A0\u00A0Short"))),
             react_1["default"].createElement(antd_1.Divider, null),
-            react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__detail'] },
+            react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__describe'] },
                 react_1["default"].createElement("h2", null, "Meet Luna"),
                 react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__detail__content'] },
                     react_1["default"].createElement("p", null, "This is Luna. He is one of the trio rescued from outdoors. He is a little shy like his brother, Poppy but is still playful and cuddly. He would really like to be adopted with one of his siblings if possible or to a home with another young cat."),
-                    react_1["default"].createElement("p", null, "Poppy, Piper and Luna are available for adoption through Grand River All Breed Rescue. The adoption donation is $200 per cat. All of our cats are spayed or neutered, microchipped, and vaccinated as part of their adoption. If you\u2019d like to adopt, please fill out an application. You can also reach us by email at GrandRiverRescue@hotmail.com.")))),
+                    react_1["default"].createElement("p", null, "Poppy, Piper and Luna are available for adoption through Grand River All Breed Rescue. The adoption donation is $200 per cat. All of our cats are spayed or neutered, microchipped, and vaccinated as part of their adoption. If you\u2019d like to adopt, please fill out an application. You can also reach us by email at GrandRiverRescue@hotmail.com."))),
+            react_1["default"].createElement(antd_1.Divider, null),
+            react_1["default"].createElement(antd_1.Button, { className: PetPage_module_scss_1["default"]['pet__button'] },
+                "Adopt Luna ",
+                react_1["default"].createElement(icons_1.ArrowRightOutlined, { className: PetPage_module_scss_1["default"]['pet__button__arrow'] }))),
         react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__side'] },
-            react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__photo'] }),
-            react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__sponsor'] }))));
+            react_1["default"].createElement("div", { className: PetPage_module_scss_1["default"]['pet__side__sponsor'] },
+                react_1["default"].createElement("h4", null, "Help provide for Luna\u2019s Care"),
+                react_1["default"].createElement(antd_1.Button, { className: PetPage_module_scss_1["default"]['button'] }, "Sponsor Luna")))));
 };
